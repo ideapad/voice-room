e9b0011d7d02ed1183992ce60eb86d99a876c1a3
Viktor Manchak <svaljava@gmail.com>
2020-10-11 18:32:36 +0300

    Add Angular (#4)
    * feat(angular): create angular project
    
    Co-authored-by: Manchak, Victor <Victor.Manchak@wolterskluwer.com>

--- src/WebUI/Controllers/WeatherForecastController.cs
+++ src/WebUI/Controllers/WeatherForecastController.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace VoiceRoom.API.Controllers
+namespace WebUI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
